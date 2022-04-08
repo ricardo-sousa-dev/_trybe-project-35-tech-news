d541410b04f8aa077c8de6cfbe90b55803b54d64
ricardo-sousa <rwmsousa@gmail.com>
2022-04-08 16:13:28 -0300

    feat: requirement 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -18,18 +18,36 @@ def fetch(url, timeout=3):
     return response.text  # retorna o conteúdo HTML
 
 
-html_content = fetch(url_da_noticia)
+html_content = fetch(url_da_noticia)  # Armazena o html da page
 # print(html_content)
 
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    # Parseia o HTML
+    page = parsel.Selector(html_content)
+    list_news = []
+    # for pega todos os artigos da página
+    for article in page.css("div.tec--list__item"):
+        # pega a url do artigo
+        url_notice = article.css("a.tec--card__title__link::attr(href)").get()
+        # # pega o título do artigo
+        # title = article.css("a.tec--card__title__link::text").get()
+        # # pega a data do artigo
+        # date = article.css("span.tec--card__date::text").get()
+        # # adiciona a notícia na lista
+        # list_news.append({"url": url_notice, "title": title, "date": date})
+        list_news.append(url_notice)
+
+    return list_news
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
+
+
+print(scrape_novidades(html_content))
 
 
 # Requisito 4
