a265bcff63df469270e5d963ac067f6627f1e748
ricardo-sousa <rwmsousa@gmail.com>
2022-04-13 09:57:28 -0300

    feat: requirement 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,8 @@
+import re
 import requests  # importa o módulo requests para fazer a requisição HTTP
 import time  # importa o módulo time para simular um delay
 from parsel import Selector  # importa o módulo parsel para fazer parse do HTML
+import database  # módulo database para inserir os dados no banco de dados
 
 
 url_da_noticia = "https://www.tecmundo.com.br/novidades"
@@ -20,7 +22,6 @@ def fetch(url, timeout=3):
 
 
 html_content = fetch(url_da_noticia)  # Armazena o html da page
-# print(html_content)
 
 
 # Requisito 2
@@ -57,13 +58,44 @@ def scrape_noticia(html_content):
 
     news = {}
     news['url'] = selector.css("link[rel=canonical]::attr(href)").get()
-    news['title'] = selector.css("h1.tec--article__header__title::text").get()
-    news['timestamp'] = selector.css(
-        "time.tec--article__header__date::text").get()
+    news['title'] = selector.css("#js-article-title::text").get()
+    news['timestamp'] = selector.css("#js-article-date::attr(datetime)").get()
+
+    writer = selector.css("a.tec--author__info__link::text").get()
+    if writer is None:
+        writer = selector.css(
+            "div.tec--timestamp div:nth-child(2) a::text").get()
+        if writer is None:
+            writer = selector.css("p.z--m-none::text").get()
+    news['writer'] = writer.strip() if writer is not None else None
+
+    shares_count = selector.css("div.tec--toolbar__item::text").get()
+    if shares_count:
+        # https://pythonexamples.org/python-regex-extract-find-all-the-numbers-in-string/
+        shares_count = int(re.findall(r'\d+', shares_count)[0])
+    else:
+        shares_count = 0
+    news['shares_count'] = shares_count
+
+    news['comments_count'] = int(
+        selector.css("#js-comments-btn::attr(data-count)").get()
+        )
+    # https://stackoverflow.com/questions/64402251/difference-between-get-and-getall-in-scrapy-and-interpreting-code-output
+    # https://www.w3schools.com/python/ref_string_join.asp
+    news['summary'] = ''.join(selector.css(
+        "div.tec--article__body > p:nth-child(1) *::text").getall()
+        )
+    # https://www.w3schools.com/python/ref_string_strip.asp
+    sources = selector.css("div.z--mb-16 div a::text").getall()
+    news['sources'] = [source.strip() for source in sources]
+
+    categories = selector.css("div#js-categories a::text").getall()
+    news['categories'] = [category.strip() for category in categories]
 
     return news
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    """Retorna uma lista de notícias"""
+
