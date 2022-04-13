bd39e9b35c21e541267f10f88c9004c3815cf98d
ricardo-sousa <rwmsousa@gmail.com>
2022-04-13 11:25:19 -0300

    feat: requirement 6 and 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,38 @@
+import datetime
+from tech_news.database import db
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_list = []
+    # https://www.guru99.com/regular-expressions-mongodb.html
+    for news in db.news.find({"title": {"$regex": title, "$options": "i"}}):
+        new = news['title'], news['url']
+        news_list.append(new)
+    return news_list
+
+
+def date_validator(string):
+    try:
+        datetime.datetime.strptime(string, '%Y-%m-%d')
+        return True
+    except ValueError:
+        return False
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    date_validation = date_validator(date)
+    news_list = []
+
+    if date_validation:
+        for new in db.news.find({
+                "timestamp": {"$regex": date, "$options": "i"}}):
+            notice = new["title"], new["url"]
+            news_list.append(notice)
+        return news_list
+    else:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
